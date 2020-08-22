23a9c3a29bcc35dac54edb6d0e2c8d0a45af6d7b
Anca Simon <ancarsimon@gmail.com>
2020-08-22 15:01:36 -0500

    Add project files.

---
+++ CorporateClass/Company.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClass
+{
+    class Company
+    {
+        // Some properties
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a private property for holding a collection of current employees --> so initially, this was a private property; 
+        //private List<Employee> Employees { get; set; }
+
+        //then #2 in the ex requires that you make changes to allow users to assign employees to the company --> so: 
+        public List<Employee> Employees { get; set;}
+
+
+        // Create a method that allows a caller to add an employee
+        public void AddEmployee(Employee newEmployee)
+        {
+            Employees.Add(newEmployee);
+        }
+
+        // Create a method that allows a caller to remove an employee
+        public void RemoveEmployee(Employee currentEmployee)
+        {
+            Employees.Remove(currentEmployee);
+        }
+
+        // Create a method that allows a caller to retrieve the list of employees
+        public void ListEmployees(List<Employee> listOfEmployees)
+        {
+            Console.WriteLine("Our current employees are: ");
+            foreach (var person in listOfEmployees)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+        public Company(string name, DateTime createdOn)
+        {
+            Name = name;
+            CreatedOn = createdOn;
+        }
+    }
+}
---
+++ CorporateClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companyA = new Company("Company A", new DateTime(2020, 07, 01));
+
+            var employee1 = new Employee("Anna B", "receptionist", new DateTime(2020, 07, 01));
+            var employee2 = new Employee("Georgie S", "CEO", new DateTime(2020, 07, 01));
+            var employee3 = new Employee("Carlee N", "manager", new DateTime(2020, 07, 03));
+
+            List<Employee> firstEmployees = new List<Employee>();
+            firstEmployees.Add(employee1);
+            firstEmployees.Add(employee2);
+            firstEmployees.Add(employee3);
+
+            companyA.Employees = firstEmployees;
+
+            companyA.ListEmployees(companyA.Employees);
+        }
+    }
+}
